76f250878dfd2a682373cbd3c7e5a928b61da66b
Alexei Mamani <alexeimc763@gmail.com>
2023-09-18 12:22:41 -0500

    Start repo

---
+++ first_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.purple, Colors.deepPurple],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: const Center(
+          child: Text(
+            "Hello from Flutter",
+            style: TextStyle(color: Colors.white, fontSize: 28.0),
+          ),
+        ),
+      ),
+    ),
+  ));
+}
